20fe200b3ed5a0853d8b0bb5699f816c08dd07f5
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-02-24 18:46:27 -0600

    completed python conditionals and flow control tutorial

--- Learn Python/conds_and_cont_flow.py
+++ Learn Python/conds_and_cont_flow.py
@@ -1,3 +1,8 @@
+# comparators
+# boolean operators
+# conditional statements
+
+
 # conditionals False and True but be capital
 x = True
 
@@ -5,3 +10,27 @@ print (x)
 
 # and, or, must be lower-case
 print ("cat" == "dog" or "cat" == "cat")
+
+
+# Complete the if and elif statements!
+def grade_converter(grade):
+    if grade >= 90:
+        return "A"
+    elif 80 <= grade <= 89:
+        return "B"
+    elif 70 <= grade <= 79:
+        return "C"
+    elif 65 <= grade <= 69:
+        return "D"
+    else:
+        return "F"
+
+
+# This should print an "A"
+print grade_converter(92)
+
+# This should print a "C"
+print grade_converter(70)
+
+# This should print an "F"
+print grade_converter(61)
