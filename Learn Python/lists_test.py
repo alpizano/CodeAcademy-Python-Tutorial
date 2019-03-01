211a82c7afb29e7c76287dfc43290c86149c26d0
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-02-28 23:44:48 -0600

    added lists test code

---
+++ Learn Python/lists_test.py
@@ -0,0 +1,14 @@
+my_list = [1, 9, 3, 8, 5, 7]
+
+for variable in my_list:
+    print (2 * variable)
+
+start_list = [5, 3, 1, 2, 4]
+square_list = []
+
+for variable in start_list:
+    square_list.append(variable ** 2)
+
+square_list.sort()
+
+print (square_list)
