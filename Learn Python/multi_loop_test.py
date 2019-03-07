802f5890105a9b81dce5d759c1cdafd68f08c588
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-03-06 23:39:04 -0600

    testing nested for loop with function in python accessing i variable

--- Learn Python/multi_loop_test.py
+++ Learn Python/multi_loop_test.py
@@ -1,7 +1,8 @@
-def test(x)
+def test(x,i):
     return x*i
 
 x_list = [1,2,3,4,5]
 
 for i in range(6):
-    for x in zip 
+    for x in x_list:
+        print (test(x)) # loop can access i var inside function
