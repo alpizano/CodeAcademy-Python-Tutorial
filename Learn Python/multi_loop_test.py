004e69bf9370b46cc65d2241d27fe9ece0e56ecc
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-03-09 23:46:07 -0600

    testing nested for loops

--- Learn Python/multi_loop_test.py
+++ Learn Python/multi_loop_test.py
@@ -1,4 +1,4 @@
-def test(x,i):
+def test(x):
     return x*i
 
 x_list = [1,2,3,4,5]
@@ -6,3 +6,15 @@ x_list = [1,2,3,4,5]
 for i in range(6):
     for x in x_list:
         print (test(x)) # loop can access i var inside function
+
+
+y_list = [1,5,3,7,1]
+tot = 0
+
+for k in len(y_list):
+    tot += y_list[k]
+    print (tot)
+
+
+
+
