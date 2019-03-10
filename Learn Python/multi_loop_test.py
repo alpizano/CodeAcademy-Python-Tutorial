714a5f2138cf10b28a21c0eda40d2230d1526a46
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-03-10 17:02:14 -0500

    testing python for loop enumerate for printing out index

--- Learn Python/multi_loop_test.py
+++ Learn Python/multi_loop_test.py
@@ -10,11 +10,19 @@ for i in range(6):
 
 y_list = [1,5,3,7,1]
 tot = 0
+avg = 0
+
 print ("Length of y list is: %s " % len(y_list))
-for k in y_list:
+for idx,k in enumerate(y_list):
     tot += k
-    print (tot)
+    avg = tot/len(y_list)
+    print ("At iteration %s, total of y_list is: %s" % (idx,tot))
+print ("The manual average of y_list is: %s" % avg)
+print ("The final total of y_list is: %s" % tot)
 
+the_sum = sum(y_list)
+the_avg = the_sum/len(y_list)
 
-
+print ("The sum of y_list is: %s" % the_sum)
+print ("The avg of y_list using built in funct is: %s" % the_avg)
 
