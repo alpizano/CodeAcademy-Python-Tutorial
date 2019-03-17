13d46500247949a2aecd175fdff303fdae219184
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-03-16 23:43:09 -0500

    while loop test

---
+++ Learn Python/while_loop_test.py
@@ -0,0 +1,8 @@
+count = 0
+
+if count < 5:
+  print ("Hello, I am an if statement and count is %s" %count)
+
+while count < 10:
+  print ("Hello, I am a while and count is %s" % count)
+  count += 1
