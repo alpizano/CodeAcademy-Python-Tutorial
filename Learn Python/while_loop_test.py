f79306787901879d212b69c9486765015daa9e10
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-03-29 23:19:13 -0500

    testing while loop python

--- Learn Python/while_loop_test.py
+++ Learn Python/while_loop_test.py
@@ -1,15 +1,21 @@
-import numpy as np
+
 
 count = 0
 
-if count < 5:
-  print ("Hello, I am an if statement and count is %s" %count)
+#if count < 5:
+  #print ("Hello, I am an if statement and count is %s" %count)
 
-while count < 10:
-  print ("Hello, I am a while and count is %s" % count)
-  count += 1
+#while count < 10:
+  #print ("Hello, I am a while and count is %s" % count)
+  #count += 1
 
-  blah = np.random.permutation(50)
+  #blah = np.random.permutation(50)
 
-for i in blah[:25]:
-  print (i)
+#for i in blah[:25]:
+  #print (i)
+
+choice = input('Enjoying the course? (y/n)')
+
+while (choice != 'n' and choice != 'y'):
+  choice = input("Sorry, I didn't catch that. Enter again: ")
+
