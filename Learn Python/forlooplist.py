3f142ede0936b9b5f6fa777e4a55cba395d6d7d7
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-04-19 00:24:25 -0500

    added for loop list tutorial

---
+++ Learn Python/forlooplist.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+
+hobbies = []
+
+# Add your code below!
+for i in range(3):
+	hobby = input("What is your hobby?")
+	hobbies.append(hobby)
+
+print (hobbies)
+print ("fuck you man")
